611d495a962d1d46c3ba2d577d3b127d398302a5
HWANG JAEYEON <woduseh@gmail.com>
2022-07-26 16:56:49 +0900

    Initial commit

---
+++ src/main/java/com/hac/duohalle/DuohalleApplication.java
@@ -0,0 +1,13 @@
+package com.hac.duohalle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuohalleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuohalleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hac/duohalle/DuohalleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hac.duohalle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuohalleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
