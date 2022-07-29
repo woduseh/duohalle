72d48fd36015ae5d948324740c62c612608c6d59
HWANG JAEYEON <woduseh@gmail.com>
2022-07-29 10:56:38 +0900

    Chore: 불필요한 파일 삭제 및 파일 이름 변경

--- src/main/java/com/hac/duohalle/DuoHalleApplication.java
+++ src/main/java/com/hac/duohalle/DuoHalleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DuohalleApplication {
+public class DuoHalleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DuohalleApplication.class, args);
+        SpringApplication.run(DuoHalleApplication.class, args);
     }
 
 }
