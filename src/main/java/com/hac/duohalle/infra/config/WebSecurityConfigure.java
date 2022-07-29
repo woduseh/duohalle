6a795e1e570b7bf6d139fed85497896e6a93cc57
HWANG JAEYEON <woduseh@gmail.com>
2022-07-29 09:46:24 +0900

    Feat: Thymeleaf를 이용한 페이지 레이아웃 틀 잡아두기

---
+++ src/main/java/com/hac/duohalle/infra/config/WebSecurityConfigure.java
@@ -0,0 +1,53 @@
+package com.hac.duohalle.infra.config;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfigure {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)
+            throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/js/**", "/css/**", "/img/**", "/fonts/**", "/vendor/**")
+                .permitAll()
+                .antMatchers("/", "/login", "/sign-up", "/check-email", "/check-email-token",
+                        "/email-login", "/check-email-login", "/login-link")
+                .permitAll()
+                .mvcMatchers(HttpMethod.GET, "profile/*")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .disable()
+                .csrf()
+                .disable()
+                .headers()
+                .disable()
+                .httpBasic()
+                .disable()
+                .rememberMe()
+                .disable()
+                .logout()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling();
+
+        return http.build();
+    }
+}
