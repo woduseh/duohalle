1b9498e0dc0a3623c70feb6cd931b1bf7e080e95
HWANG JAEYEON <woduseh@gmail.com>
2022-08-04 10:48:07 +0900

    Feat: @mdi/font를 통한 아이콘, jdenticon을 이용한 기본 프로필 아이콘

--- src/main/java/com/hac/duohalle/domain/account/dto/request/AccountSignUpRequestDto.java
+++ src/main/java/com/hac/duohalle/domain/account/dto/request/AccountSignUpRequestDto.java
@@ -5,6 +5,7 @@ import static com.hac.duohalle.infra.config.WebSecurityConfig.getPasswordEncoder
 import com.hac.duohalle.domain.account.entity.Account;
 import javax.persistence.Column;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
@@ -12,13 +13,17 @@ import org.hibernate.validator.constraints.Length;
 @Data
 public class AccountSignUpRequestDto {
 
+    @NotBlank
     @Column(unique = true)
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
+
+    @NotBlank
     @Column(unique = true, length = 20)
     @Length(min = 2, max = 20, message = "닉네임은 2자 이상 20자 이하여야 합니다.")
     private String nickname;
 
+    @NotBlank
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d).{8,20}$", message = "비밀번호는 영문, 숫자, 특수문자를 포함하여 8자 이상 20자 이하여야 합니다.")
     @Column(length = 20)
     private String password;
